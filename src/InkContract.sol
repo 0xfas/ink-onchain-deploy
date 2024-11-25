b445b3b57b5d0d4bf2be19fe8b3e283a51bc0fed
0xFAS <0xfas.tesnet@gmail.com>
2024-11-25 20:09:55 +0700

    Update

---
+++ src/InkContract.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+contract Fas {
+    string public greeting = "Hello, 0xFas!";
+ 
+    function setGreeting(string memory _greeting) public {
+        greeting = _greeting;
+    }
+}
